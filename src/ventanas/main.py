8c3b558e3ab7b5639ba2d6a95a70f3687f9d0c93
MartaCanino <martacaninoromero@gmail.com>
2023-09-30 13:16:45 +0200

    Funciona main bien con Login e imagenes

--- src/ventanas/main.py
+++ src/ventanas/main.py
@@ -1,10 +1,14 @@
 import sys
-from PyQt5.QtWidgets import QApplication
-from login import Ui_Form
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from login import Ui_ContainerLogin
+
+class ContainerLogin(QMainWindow, Ui_ContainerLogin):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = Ui_Form()
-    window.setupUi(window)
+    window = ContainerLogin()
     window.show()
     sys.exit(app.exec_())
