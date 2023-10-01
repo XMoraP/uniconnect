afe49ae60f4e2370298f72bfd78f5c3f842c7a65
MartaCanino <martacaninoromero@gmail.com>
2023-10-01 16:42:49 +0200

    Funciona boton de iniciar sesion desde ventana principal

--- src/main.py
+++ src/main.py
@@ -3,10 +3,22 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 from ui.principal import Ui_Form
 import ui.imagenes.resources_rc
 
+from ui.login import Ui_ContainerLogin
+
+class LoginWindow(QMainWindow, Ui_ContainerLogin):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
 class MainWindow(QMainWindow, Ui_Form):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
+        self.iniciarsesion.clicked.connect(self.open_login_window)
+
+    def open_login_window(self):
+        self.login_window = LoginWindow()
+        self.login_window.show()
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
