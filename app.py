ca3b665d4c1fa01f7c91f7b1d792a1d0a2b15cfe
juanGrimaS <juanGrimaS@users.noreply.github.com>
2023-10-26 23:10:37 +0200

    Tutor conectado a otras ventanas

--- app.py
+++ app.py
@@ -236,7 +236,7 @@ def contact():
                 os.remove(temp_image_path)
         print("Imagenes borradas")
 
-    cur.execute("SELECT CONCAT(user.nombre, ' ', user.apellido) AS nombre_apellido, user.email, tutor.asignaturas_tutor, image.image FROM user INNER JOIN tutor ON user.id_user = tutor.id_tutor LEFT JOIN image ON user.id_user = image.id_user;")
+    cur.execute("SELECT * FROM vista_ventana_tutores")
     contacts = cur.fetchall()
     cur.close()
     contacts_list = []
