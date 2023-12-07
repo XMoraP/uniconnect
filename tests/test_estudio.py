d762293643c0dea5ffd0a4b1fd208510a99e3a7c
MartaCanino <martacaninoromero@gmail.com>
2023-12-07 20:30:36 +0100

    Crear segundo test para estudio

--- tests/test_estudio.py
+++ tests/test_estudio.py
@@ -38,3 +38,34 @@ def test_estudio(client, mock_db):
     ), follow_redirects=True)
 
     assert response.status_code == 200
+
+
+def test_estudio_form_submission(client, mock_db):
+    # Set up Flask session data
+    with client.session_transaction() as session:
+        session['name'] = 'John'
+        session['last_name'] = 'Doe'
+        session['email'] = 'john.doe@email.com'
+        session['status'] = 'Tutor'
+        session['nombre_grado'] = 'Ingenieria en Sistemas'
+        session['photo_url'] = 'static/images/userPhoto.png',
+        session['id_user'] = 1
+
+    mock_cursor = mock_db.cursor.return_value
+    mock_cursor.fetchall.return_value = []  # No existing study groups
+
+    # Configure fetchone to return a dictionary-like object
+    mock_cursor.fetchone.return_value = {'conteo': 0}
+
+    response = client.post('/estudio', data=dict(
+        title='Estudiar PROLOG',
+        subject='Inteligencia Artificial',
+        description='Estudiar PROLOG para el examen',
+        location='Biblioteca',
+        days='13/01/2024',
+        time='12:00'
+    ), follow_redirects=True)
+
+    assert response.status_code == 200
+
+
