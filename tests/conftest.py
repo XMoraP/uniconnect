ccd055a132423e0db146f6b0ec5f716d381ce73b
Dulibeth <dulibeth.medinachirinos@usp.ceu.es>
2023-12-07 19:04:51 +0100

    Base de datos mock en los test

---
+++ tests/conftest.py
@@ -0,0 +1,27 @@
+# conftest.py
+
+import pytest
+import os
+from unittest.mock import patch, Mock
+from flask import Flask
+from dotenv import load_dotenv
+from flask_mysqldb import MySQL
+
+load_dotenv()
+@pytest.fixture
+def mock_db():
+    # Create a mock for the database connection
+    mock_database = Mock()
+
+    # Create a Flask app for obtaining an application context
+    app = Flask(__name__)
+    app.config['MYSQL_HOST'] = os.getenv("DB_HOST")  
+    app.config['MYSQL_USER'] = os.getenv("DB_USER")
+    app.config['MYSQL_PASSWORD'] = os.getenv("DB_PASSWORD")
+    app.config['MYSQL_DB'] = os.getenv("DB")
+    app.config['MYSQL_CURSORCLASS'] = os.getenv("CURSOSRCLASS")
+    MySQL(app)
+
+    # Decorate the function with patch to replace the real database connection with the mock
+    with patch('flask_mysqldb.MySQL.connection', mock_database):
+        yield mock_database
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -21,24 +21,6 @@ def client():
     client = app.test_client()
     yield client
 
-@pytest.fixture
-def mock_db():
-    # Create a mock for the database connection
-    mock_database = Mock()
-
-    # Create a Flask app for obtaining an application context
-    app = Flask(__name__)
-    app.config['MYSQL_HOST'] = os.getenv("DB_HOST")  
-    app.config['MYSQL_USER'] = os.getenv("DB_USER")
-    app.config['MYSQL_PASSWORD'] = os.getenv("DB_PASSWORD")
-    app.config['MYSQL_DB'] = os.getenv("DB")
-    app.config['MYSQL_CURSORCLASS'] = os.getenv("CURSOSRCLASS")
-    MySQL(app)
-
-    # Decorate the function with patch to replace the real database connection with the mock
-    with patch('flask_mysqldb.MySQL.connection', mock_database):
-        yield mock_database
-
 def test_agregar_user(client, mock_db):
     # Configure the expected behavior of the mock (simulate a database result)
     mock_cursor = mock_db.cursor.return_value
